fcd0b268289aca93d815126d8789c7d699c6c68d
adammc331 <amcneilly331@gmail.com>
2022-09-21 19:11:24 -0400

    Adding necessary hilt annotation.

--- app/src/main/java/com/adammcneilly/rustybeer/MainActivity.kt
+++ app/src/main/java/com/adammcneilly/rustybeer/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import com.adammcneilly.rustybeer.beerstyle.BeerStyleListScreen
 import com.adammcneilly.rustybeer.theme.RustyBeerTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
